2f392a438bc925e6417f8093c8e199c3a2320bfa
Alastair Harrison <aharrison24@gmail.com>
2016-01-26 22:28:12 +0100

    Fix stdint inclusion under clang

--- src/ORBmatcher.cc
+++ src/ORBmatcher.cc
@@ -27,7 +27,11 @@
 
 #include "Thirdparty/DBoW2/DBoW2/FeatureVector.h"
 
-#include<stdint-gcc.h>
+#if defined(__clang__)
+  #include <stdint.h>
+#else
+  #include <stdint-gcc.h>
+#endif
 
 using namespace std;
 
